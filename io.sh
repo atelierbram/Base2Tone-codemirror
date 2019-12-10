66b2e3d0a21c4f97b7b1f264f19a183fa8318d38
atelierbram <bramdeh@gmail.com>
2019-12-10 19:29:00 +0100

    four new themes: Field, Mall, Motel & Porch

--- io.sh
+++ io.sh
@@ -35,6 +35,14 @@ overwrite_to_file()
   base16-builder --scheme "db/schemes/base2tone-lavender.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-lavender-light.css"
   base16-builder --scheme "db/schemes/base2tone-garden.yml" --template "db/templates/dark.ejs" > "docs/CodeMirror/theme/base2tone-garden-dark.css"
   base16-builder --scheme "db/schemes/base2tone-garden.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-garden-light.css"
+  base16-builder --scheme "db/schemes/base2tone-mall.yml" --template "db/templates/dark.ejs" > "docs/CodeMirror/theme/base2tone-mall-dark.css"
+  base16-builder --scheme "db/schemes/base2tone-mall.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-mall-light.css"
+  base16-builder --scheme "db/schemes/base2tone-porch.yml" --template "db/templates/dark.ejs" > "docs/CodeMirror/theme/base2tone-porch-dark.css"
+  base16-builder --scheme "db/schemes/base2tone-porch.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-porch-light.css"
+  base16-builder --scheme "db/schemes/base2tone-field.yml" --template "db/templates/dark.ejs" > "docs/CodeMirror/theme/base2tone-field-dark.css"
+  base16-builder --scheme "db/schemes/base2tone-field.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-field-light.css"
+  base16-builder --scheme "db/schemes/base2tone-motel.yml" --template "db/templates/dark.ejs" > "docs/CodeMirror/theme/base2tone-motel-dark.css"
+  base16-builder --scheme "db/schemes/base2tone-motel.yml" --template "db/templates/light-alt.ejs" > "docs/CodeMirror/theme/base2tone-motel-light.css"
  }
 
 # execute it
